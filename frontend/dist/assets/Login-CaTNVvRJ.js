6095c95fae8af9e524df279647f1bfd10f5b8e6a
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-05-25 12:24:57 +0500

    Add files via upload

---
+++ frontend/dist/assets/Login-CaTNVvRJ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B2f3zDIP.js";import{A as t}from"./AuthPage-wLr_s0Im.js";function n(){return o.jsx(t,{initialMode:"login"})}export{n as default};
---
+++ frontend/dist/assets/Register-0vgAIQXW.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B2f3zDIP.js";import{A as r}from"./AuthPage-wLr_s0Im.js";function o(){return t.jsx(r,{initialMode:"register"})}export{o as default};
