ef0eff6da71f70b9e40df8857f96f4f9fec091ab
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-06-02 13:16:58 +0500

    Delete check_db2.js

--- check_db2.js
+++
@@ -1,3 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('backend/myshop.db');
-console.log(db.prepare("SELECT sql FROM sqlite_master WHERE name='expeditor_closed_batches'").get().sql);
