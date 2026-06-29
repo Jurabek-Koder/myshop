1522304a5307c378968ce2b930112e2c3821ad9d
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-06-29 09:54:46 +0500

    Delete frontend/dist directory

--- frontend/dist/assets/Login-DRNqi_qL.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-WdfQJ-11.js";import{A as t}from"./AuthPage-CSbPmu5C.js";import"./authRedirect-Ba61NrRM.js";function a(){return o.jsx(t,{initialMode:"login"})}export{a as default};
