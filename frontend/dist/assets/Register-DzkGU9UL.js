2445d55565894f98fd82b7ca3070b411de493401
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-08-28 02:10:40 +0500

    Add files via upload

---
+++ frontend/dist/assets/Register-DzkGU9UL.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BMbMWyvy.js";import{A as r}from"./AuthPage-PG7wMjgu.js";import"./authRedirect-DzvDpOzU.js";function s(){return t.jsx(r,{initialMode:"register"})}export{s as default};
