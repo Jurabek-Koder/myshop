c3dde50e4ac7c2cc4e0f6428aa92ab1ebde44f70
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-08-29 04:59:40 +0500

    Delete frontend directory

--- frontend/dist/assets/Login-BVQb1SXZ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BMbMWyvy.js";import{A as t}from"./AuthPage-PG7wMjgu.js";import"./authRedirect-DzvDpOzU.js";function a(){return o.jsx(t,{initialMode:"login"})}export{a as default};
