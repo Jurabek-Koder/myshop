dae7fb44ec9a23317287ca89f5c3478b73da9b06
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-09-05 15:01:57 +0500

    Delete frontend directory

--- frontend/dist/assets/Login-Zps9dPqj.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DIUlTeCf.js";import{A as t}from"./AuthPage-4ofsMPnq.js";import"./authRedirect-Dx6_Bl3m.js";function a(){return o.jsx(t,{initialMode:"login"})}export{a as default};
--- frontend/src/pages/Login.jsx
+++
@@ -1,5 +0,0 @@
-import AuthPage from './AuthPage.jsx';
-
-export default function Login() {
-  return <AuthPage initialMode="login" />;
-}
