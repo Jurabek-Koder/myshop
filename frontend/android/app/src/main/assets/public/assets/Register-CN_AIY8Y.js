ce053e849018e77047faf08c552f5156b84c2759
Jurabek-Koder <61523486+Jurabek-Koder@users.noreply.github.com>
2026-09-05 16:01:17 +0500

    Delete frontend/android/app/src/main/assets directory

--- frontend/android/app/src/main/assets/public/assets/Register-CN_AIY8Y.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BqJ06dPb.js";import{A as r}from"./AuthPage-CcdZYf_k.js";import"./authRedirect-4DUhkrQX.js";function s(){return t.jsx(r,{initialMode:"register"})}export{s as default};
--- frontend/android/app/src/main/assets/public/assets/index-Dv51i_-V.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CgI7KbW1.js","assets/index-CKZhs57K.js"])))=>i.map(i=>d[i]);
-import{_ as p}from"./index-BqJ06dPb.js";import{registerPlugin as r}from"./index-CKZhs57K.js";const _=r("App",{web:()=>p(()=>import("./web-CgI7KbW1.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
